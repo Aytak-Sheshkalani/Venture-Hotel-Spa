5bd55e37d35f7460ed46468d9399990fdc764ec6
Hulya07 <90285905+Hulya07@users.noreply.github.com>
2022-04-19 03:24:56 -0400

    Registration page files

---
+++ registration-form/message.php
@@ -0,0 +1,25 @@
+<?php
+  $name = htmlspecialchars($_POST['name']);
+  $lastname = htmlspecialchars($_POST['lastname']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\n LastName: $lastname \nPhone: $phone\nEmail: $email\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your registration has been successfully completed!";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
+?>
