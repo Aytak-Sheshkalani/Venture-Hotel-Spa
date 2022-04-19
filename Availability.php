589300c84efbc7191df487a9adcfa5957fc46d4c
Mroknsharifi <90482394+Mroknsharifi@users.noreply.github.com>
2022-04-19 08:26:29 -0400

    Add files via upload
    amin

--- Availability.php
+++ Availability.php
@@ -31,8 +31,6 @@ else
    echo "<p style='font-size: large; color:red; font-size: large'>Enter Room Type.</p></br>";
 }
 
-
-
  
 $q = "UPDATE Room AS R INNER JOIN Booking AS B SET occupied=1 WHERE ('$date1' <= dateTo AND '$date2' >= dateFrom) AND R.roomID = B.roomID";
 $booked = mysqli_query($dbc,$q);
@@ -45,19 +43,9 @@ $que = "SELECT COUNT(*) FROM Room WHERE occupied ='0';";
 $notBookedRows = mysqli_query($dbc,$qu);
 while ($notBookedOb = mysqli_fetch_assoc($notBooked)) 
 {
-    echo "Room No " . $notBookedOb['roomID'] . " is free for this booking. <br />";
+    echo "<div style='background-color:gold; witdth: 200px; margin-top: 10%'>
+     <h2 style='text-align:center; padding: 20px'> Room No " . $notBookedOb['roomID'] .
+      " is free for this booking. <br/> </h2> </div> ";
 }
-
-// function dateCheck($date1, $date2) {
-//     global $mysqli;
-//     global $roomType;
-//     $booked = $mysqli->query("UPDATE Rooms AS R INNER JOIN Bookings AS B SET occupied=1 WHERE ('$date1' <= dateTo AND '$date2' >= dateFrom) AND R.roomID = B.roomID") or die($mysqli->error);
-//     $bookedRows = $booked->num_rows;
-//     $notBooked = $mysqli->query("SELECT * FROM Rooms AS R WHERE occupied != 1 AND R.roomType = '$roomType'");
-//     $notBookedRows = $notBooked->num_rows;
-//     while($notBookedOb = $notBooked->fetch_object()) {
-//         echo "Room No " . $notBookedOb->roomID . " is free for this booking. <br />"; 
-//     }
-// }
 
 ?>
